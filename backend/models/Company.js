05501853f2387cfee60d9cb49752c9d295aa008d
Badri Prasad <badri2001prasad@gmail.com>
2026-06-15 17:25:46 +0530

    Update Company.js
    for company sorting

--- backend/models/Company.js
+++ backend/models/Company.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
 const companySchema = new mongoose.Schema({
-  name: { type: String },
+  name: { type: String, required: true },
   vertical: { type: String },
   status: { type: String },
   desc: { type: String },
-  websiteUrl: { type: String, default: '' }, // <--- URL Field
+  websiteUrl: { type: String, default: '' },
   team: [{
     memberName: { type: String },
     designation: { type: String },
     image: { type: String }
   }],
-  isDeleted: { type: Boolean, default: false }
+  isDeleted: { type: Boolean, default: false },
+  priority: { type: Number, default: 999 } // 🔥 Naya field dynamic sorting ke liye
 }, { timestamps: true });
 
 module.exports = mongoose.model('Company', companySchema);
